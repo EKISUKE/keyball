d8ea07923e560fd060481fb95a77fd9c716d8a0f
EKISUKE <yahoo622800@gmail.com>
2025-11-06 03:22:38 +0900

    Tapping Term中に他のキーが押された場合にホールドを優先する設定を追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
@@ -53,6 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define TAPPING_FORCE_HOLD  // 34バイト
 // 下記三つで444バイト
 #define TAPPING_TERM 175
+#define HOLD_ON_OTHER_KEY_PRESS // Tapping Term中に他のキーが押された場合にホールドを優先する
 // #define PERMISSIVE_HOLD
 // #define RETRO_TAPPING
 
