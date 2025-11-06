925f60331fec4e4833dfdd88299aa92cc00b8c7c
EKISUKE <yahoo622800@gmail.com>
2025-11-06 14:21:53 +0900

    TAPPING_TERMを175から120に変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define TAPPING_FORCE_HOLD  // 34バイト
 // 下記三つで444バイト
-#define TAPPING_TERM 175
+#define TAPPING_TERM 120
 //#define HOLD_ON_OTHER_KEY_PRESS // Tapping Term中に他のキーが押された場合にホールドを優先する
 // #define PERMISSIVE_HOLD
 // #define RETRO_TAPPING
