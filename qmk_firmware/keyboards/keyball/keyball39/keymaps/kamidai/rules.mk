c3e02e6042c495b521bc256821bc1ad0cf45b5ca
EKISUKE <yahoo622800@gmail.com>
2025-11-06 13:53:27 +0900

    DYNAMIC_TAPPING_TERM_ENABLE に関して、kamidaiさん以下のものをいじっていたので、自分のを編集

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/rules.mk
@@ -11,9 +11,6 @@ OLED_ENABLE = yes
 COMBO_ENABLE = yes
 # AUTO_SHIFT_ENABLE = yes
 
-# 動的にタッピングタームを変更可能にする
-DYNAMIC_TAPPING_TERM_ENABLE = yes
-
 # ファームウェアのサイズを削減
 EXTRAFLAGS += -flto
 GRAVE_ESC_ENABLE = no  # 90バイト
